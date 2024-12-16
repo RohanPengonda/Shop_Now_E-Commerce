367e7f189e0bc794cf354550b9f9eb7433dfbd30
RohanPengonda <rpengonda1@gmail.com>
2024-12-16 12:02:57 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Product from "./pages/Product";
 import LoginSignUp from "./pages/LoginSignUp";
 import Cart from "./pages/Cart";
 import Footer from "./components/Footer/Footer";
+import mens_banner from "./components/assets/banner_mens.png";
+import women_banner from "./components/assets/banner_women.png";
+import kids_banner from "./components/assets/banner_kids.png";
 
 function App() {
   return (
@@ -16,9 +19,18 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
-            <Route path="/mens" element={<ShopCategory category="men" />} />
-            <Route path="/womens" element={<ShopCategory category="women" />} />
-            <Route path="/kids" element={<ShopCategory category="kids" />} />
+            <Route
+              path="/men"
+              element={<ShopCategory banner={mens_banner} category="men" />}
+            />
+            <Route
+              path="/women"
+              element={<ShopCategory banner={women_banner} category="women" />}
+            />
+            <Route
+              path="/kids"
+              element={<ShopCategory banner={kids_banner} category="kids" />}
+            />
             <Route path="/product" element={<Product />}>
               <Route path=":productId" element={<Product />} />
             </Route>
--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
-const ShopCategory = () => {
-  return <div>ShopCategory</div>;
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../context/ShopContext";
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+    </div>
+  );
 };
 
 export default ShopCategory;
