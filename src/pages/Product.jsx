52d0a79fd58debedeb085e234c2f55fa59e5c52a
RohanPengonda <rpengonda1@gmail.com>
2024-12-17 22:13:33 +0530

    Updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../components/Breadcrums/Breadcrum";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_products } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_products.find((e) => e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrum product={product} />
+    </div>
+  );
 };
 
 export default Product;
